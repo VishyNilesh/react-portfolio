ede17eb17b0365dbd72554dc039dad38de1db1cd
Nilesh Kumar Vishwakarma <vnilesh108@gmail.com>
2025-08-20 13:26:48 +0530

    Mobile screen improved

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -212,7 +212,7 @@ const Resume = (props) => {
               IndiaBonds
             </a>
           }
-          subHeading={"IndiaBonds"}
+          subHeading={"Assistant Manager (IT & Software)"}
           fromDate={"2022"}
           toDate={"2023"}
         />
